e9fe5865fd7bd51c8805d8b9843c90dcddd55a80
Gustavo Meredyk <gustavo@meredyk.com.br>
2017-08-31 02:09:01 +0000

    test

--- app/Http/Controllers/Web/IssueController.php
+++ app/Http/Controllers/Web/IssueController.php
@@ -55,8 +55,6 @@ class IssueController extends Controller
                 ->sortBy('position')->groupBy('config_status_id');
         }
 
-        dd($issues);
-
         $configStatus = ConfigStatus::type('issues')->get();
 
         if (!is_null($sprint) && !count($sprint)) {
